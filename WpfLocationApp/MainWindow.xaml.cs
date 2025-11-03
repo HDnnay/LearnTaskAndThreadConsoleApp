04ca259958c172bfc31e9ba3210bdfed63ac9508
HDanny <DannyH@163.com>
2025-11-03 16:15:52 +0800

    add

---
+++ WpfLocationApp/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLocationApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateUser(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void QuiklyCreateUser(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
