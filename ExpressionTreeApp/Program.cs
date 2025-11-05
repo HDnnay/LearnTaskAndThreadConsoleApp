f104cb1836cb7e3b311a40ba86a56e8e07821289
HDanny <DannyH@163.com>
2025-11-05 18:38:08 +0800

    ExpressionTree

---
+++ ExpressionTreeApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExpressionTreeToString;
+using System.Linq.Expressions;
+var iniArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+Expression<Func<int, bool>> lambda = num => num >5;
+Expression<Func<int,int>> lambda2 = num=> num+num-1;
+var resutl = iniArray.Select(lambda2.Compile());
+Console.WriteLine(lambda2.ToString("Object notation","C#"));
+foreach (var item in resutl)
+{
+    Console.WriteLine(item);
+}
+Console.ReadLine();
---
+++ LocationSdkApp/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using LocationSDK;
+using System.Text.Json.Serialization;
+
+try
+{
+    var httpclient = new HttpClient(){Timeout = TimeSpan.FromSeconds(60) };
+    var appClient = new LoginClient(httpclient);
+    var Paramets = new LoginRequest().Parameters = new Dictionary<string, string>()
+    {
+        { "phone","13800138000" },
+        { "code","1234" }
+    };
+    var reuslt = await appClient.LoginAsync("Phone", new LoginRequest() { Parameters = Paramets });
+
+    Console.WriteLine($"{reuslt.Success}:{reuslt.Message}");
+    
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
